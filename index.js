9b95ef8329f4b195f840fa7ff6fb2eb7da420b5c
John Turner <10634270+johndturn@users.noreply.github.com>
2023-10-24 15:42:26 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const config = require('./config.json');
+const authLib = require('./lib/salesforceAuth.js');
+const SalesforceApi = require('./lib/SalesforceApi.js');
+
+main();
+
+async function main() {
+  const authToken = await authLib.authenticate(config);
+  const salesforceApi = new SalesforceApi(authToken, 'Account', config.myDomain, config.apiVersion);
+  const accountsResponse = await salesforceApi.fetchList();
+
+  console.log(accountsResponse);
+}
